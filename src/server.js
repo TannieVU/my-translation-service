95f6316999c7bc0ca9b82c24663f0f993c3c0aaa
TannieVU <warateens@gmail.com>
2025-06-12 23:17:48 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const translationRoutes = require('./api/translationRoutes');
+const { connectToServer } = require('./config/database'); // <-- THÊM DÒNG NÀY
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware để xử lý JSON body
-app.use(express.json({ limit: '50mb' })); // Tăng giới hạn để nhận file lớn
-
-// Phục vụ các file tĩnh từ thư mục 'public'
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
-
-// Sử dụng các routes API
 app.use('/api', translationRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Kết nối đến CSDL trước, sau đó mới khởi động server
+connectToServer().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
